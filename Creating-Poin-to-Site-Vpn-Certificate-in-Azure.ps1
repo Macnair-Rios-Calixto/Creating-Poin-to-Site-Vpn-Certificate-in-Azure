52c70f43634d13fee18f08a7ae724715c1977387
Macnair Rios Calixto <78324420+Macnair-Rios-Calixto@users.noreply.github.com>
2021-02-07 19:18:59 -0300

    Update Creating-Poin-to-Site-Vpn-Certificate-in-Azure.ps1

--- Creating-Poin-to-Site-Vpn-Certificate-in-Azure.ps1
+++ Creating-Poin-to-Site-Vpn-Certificate-in-Azure.ps1
@@ -13,3 +13,4 @@ New-SelfSignedCertificate -Type Custom -DnsName P2SChildCertAzure -KeySpec Signa
 -HashAlgorithm sha256 -KeyLength 2048 `
 -CertStoreLocation "Cert:\CurrentUser\My" `
 -Signer $cert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
+
