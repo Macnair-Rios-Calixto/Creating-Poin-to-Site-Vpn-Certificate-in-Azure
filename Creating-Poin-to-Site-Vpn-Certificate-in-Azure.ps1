fb3db3ea7d707ef6e8723ede4a74b443308a1f62
Macnair Rios Calixto <78324420+Macnair-Rios-Calixto@users.noreply.github.com>
2021-02-08 01:18:32 +0300

    Add files via upload

---
+++ Creating-Poin-to-Site-Vpn-Certificate-in-Azure.ps1
@@ -0,0 +1,15 @@
+#Creating-Poin-to-Site-Vpn-Certificate-in-Azure
+
+#Server Root Certificate
+$cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
+-Subject "CN=Certificado-Root" -KeyExportPolicy Exportable `
+-HashAlgorithm sha256 -KeyLength 2048 `
+-CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign
+
+
+#Client Certificate
+New-SelfSignedCertificate -Type Custom -DnsName P2SChildCertAzure -KeySpec Signature `
+-Subject "CN=Certificado-Cliente" -KeyExportPolicy Exportable `
+-HashAlgorithm sha256 -KeyLength 2048 `
+-CertStoreLocation "Cert:\CurrentUser\My" `
+-Signer $cert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
